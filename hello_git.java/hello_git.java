f6e63ad43f2463395a167690af8e484fa2ee22cd
unknown <csense2653@gmail.com>
2022-06-15 22:42:38 +0530

    added for loop

--- hello_git.java/hello_git.java
+++ hello_git.java/hello_git.java
@@ -1,7 +1,8 @@
 class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        for (int i = 0; i < 10; i++)
+            System.out.println("Hello Git");
         return;
     }
 }
