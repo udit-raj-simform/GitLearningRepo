31b5130e4d6e0607bcf0d4823fec5de7ea003ac6
unknown <csense2653@gmail.com>
2022-06-15 22:46:49 +0530

    removed for, added if

--- hello_git.java/hello_git.java
+++ hello_git.java/hello_git.java
@@ -1,7 +1,7 @@
 class HelloGit {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++)
+        if (190 % 2 == 0)
             System.out.println("Hello Git");
         return;
     }
